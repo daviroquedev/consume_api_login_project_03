d36cb1a492342751d40ff8b49de4a06ae9803ba2
Davi Roque <sowdavi@gmail.com>
2023-12-22 02:11:38 -0300

    feat: add media query when portrait

--- lib/src/modules/logged/view/user_screen.dart
+++ lib/src/modules/logged/view/user_screen.dart
@@ -19,170 +19,178 @@ class UserProfilePage extends StatelessWidget {
         title: const Text('Perfil do Usuário'),
       ),
       backgroundColor: const Color.fromARGB(255, 70, 1, 76),
-      body: Center(
-          child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            LayoutBuilder(
-              builder: (context, constraints) {
-                return SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  child: Card(
-                    elevation: 2,
-                    color: const Color.fromARGB(255, 164, 204, 228),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.only(top: 5.0),
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text(
-                                  'IDENTIFICATION CARD',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                      fontSize: 28,
-                                      fontWeight: FontWeight.bold,
-                                      fontFamily: 'Roboto'),
-                                )
-                              ]),
-                        ),
-                        Container(
-                          height: 1,
-                          color: const Color.fromARGB(255, 0, 0, 0),
-                          margin: const EdgeInsets.symmetric(vertical: 10),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
+      body: OrientationBuilder(builder: (context, orientation) {
+        return Center(
+            child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              LayoutBuilder(
+                builder: (context, constraints) {
+                  bool isVertical =
+                      orientation == Orientation.portrait ? true : false;
+
+                  return SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    child: Card(
+                      elevation: 2,
+                      color: const Color.fromARGB(255, 164, 204, 228),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(top: 5.0),
+                            child: Row(
                                 mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
                                 children: [
-                                  const SizedBox(height: 10),
-                                  Container(
-                                    width: 130,
-                                    height: 135,
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(10),
-                                        border: Border.all(
-                                          color: Colors.grey,
-                                          width: 2,
-                                        ),
-                                        color: const Color.fromARGB(
-                                            255, 2, 113, 173)),
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(8),
-                                      child: Image.network(
-                                        '${userInformation.image}',
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                  ),
                                   Text(
-                                    '${userInformation.username}',
-                                    style: const TextStyle(
+                                    'IDENTIFICATION CARD',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
                                         fontSize: 28,
                                         fontWeight: FontWeight.bold,
-                                        fontFamily: 'Assing'),
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(width: 10),
-                              Container(
-                                height: 180,
-                                child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text(
-                                        'ID ${userInformation.id}',
-                                        style: const TextStyle(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold,
+                                        fontFamily: 'Roboto'),
+                                  )
+                                ]),
+                          ),
+                          Container(
+                            height: 1,
+                            color: const Color.fromARGB(255, 0, 0, 0),
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: isVertical
+                                  ? MainAxisAlignment.center
+                                  : MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const SizedBox(height: 10),
+                                    Container(
+                                      width: 130,
+                                      height: 135,
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          border: Border.all(
+                                            color: Colors.grey,
+                                            width: 2,
+                                          ),
+                                          color: const Color.fromARGB(
+                                              255, 2, 113, 173)),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(8),
+                                        child: Image.network(
+                                          userInformation.image,
+                                          fit: BoxFit.cover,
                                         ),
                                       ),
-                                      const SizedBox(height: 1),
-                                      Text(
-                                        '${userInformation.firstName}',
-                                        style: const TextStyle(
-                                          fontSize: 26,
+                                    ),
+                                    Text(
+                                      userInformation.username,
+                                      style: const TextStyle(
+                                          fontSize: 28,
                                           fontWeight: FontWeight.bold,
+                                          fontFamily: 'Assing'),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(width: 10),
+                                SizedBox(
+                                  height: 180,
+                                  child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          'ID ${userInformation.id}',
+                                          style: const TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold,
+                                          ),
                                         ),
-                                      ),
-                                      Text('${userInformation.lastName}',
+                                        const SizedBox(height: 1),
+                                        Text(
+                                          userInformation.firstName,
                                           style: const TextStyle(
                                             fontSize: 26,
                                             fontWeight: FontWeight.bold,
-                                          )),
-                                      const SizedBox(height: 5),
-                                      Text(
-                                        '${userInformation.gender}',
-                                        style: const TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.bold,
+                                          ),
                                         ),
-                                      ),
-                                      const SizedBox(height: 5),
-                                      Text(
-                                        '${userInformation.email}',
-                                        style: const TextStyle(
+                                        Text(userInformation.lastName,
+                                            style: const TextStyle(
+                                              fontSize: 26,
+                                              fontWeight: FontWeight.bold,
+                                            )),
+                                        const SizedBox(height: 5),
+                                        Text(
+                                          userInformation.gender,
+                                          style: const TextStyle(
                                             fontSize: 18,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                    ]),
-                              ),
-                            ],
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        const SizedBox(height: 5),
+                                        Text(
+                                          userInformation.email,
+                                          style: const TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ]),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  await loginController.logout(context);
+                },
+                child: const Text('DESLOGAR'),
+                style: ButtonStyle(
+                  elevation: MaterialStateProperty.all<double>(4),
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                    const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                  ),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
                     ),
                   ),
-                );
-              },
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                await loginController.logout(context);
-              },
-              child: const Text('DESLOGAR'),
-              style: ButtonStyle(
-                elevation: MaterialStateProperty.all<double>(4),
-                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-                  const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-                ),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
+                  backgroundColor: MaterialStateProperty.all(
+                    const Color.fromARGB(225, 176, 93, 221),
+                  ),
+                  shadowColor: MaterialStateProperty.all(
+                    Colors.black.withOpacity(0.3),
+                  ),
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                    Colors.white, // Define a cor do texto como branco
                   ),
                 ),
-                backgroundColor: MaterialStateProperty.all(
-                  const Color.fromARGB(225, 176, 93, 221),
-                ),
-                shadowColor: MaterialStateProperty.all(
-                  Colors.black.withOpacity(0.3),
-                ),
-                foregroundColor: MaterialStateProperty.all<Color>(
-                  Colors.white, // Define a cor do texto como branco
-                ),
-              ),
-            )
-          ],
-        ),
-      )),
+              )
+            ],
+          ),
+        ));
+      }),
     );
   }
 }
